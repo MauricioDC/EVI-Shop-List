c11b26af1be2f4213dd409d19fa9c018c69caece
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-07-10 01:55:47 -0300

    Criando Script para Adicionar itens

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,37 @@
-
-function tabuada() {
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-        window.alert('Por favor, Digite um número!')
-    } else {
-        let n = Number(num.value)
-        let c = 1
-        tab.innerHTML = ''
-        while (c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
-        }
+let Produto = function(){
+    let nomeProduto
+    let qtdProduto
+    this.setNome = function(_nomeProduto){
+        this.nomeProduto = _nomeProduto
     }
+    this.setQtd = function(_qtdProduto){
+        this.qtdProduto = _qtdProduto
+    }
+    
+}
+
+let nmProd = document.getElementById("itemLista")
+let qtdProd = document.getElementById("qtdProduto")
+let lista = []
+
+function addProduto() {
+
+    if(nmProd.value.length == 0){
+        window.alert("Favor informar o nome do produto!")
+        nmProd.focus()
+    } 
+    else if (qtdProd.value == 0 || qtdProd.value < 0){
+        window.alert("Favor informar a quantidade!")
+        qtdProd.focus()
+    } else {
+        let produto = new Produto()
+        produto.setNome(nmProd.value)
+        produto.setQtd(qtdProd.value)
+        lista.push(produto)
+        window.alert(JSON.stringify(lista))
+        nmProd.value = ""
+        qtdProd.value = ""
+        nmProd.focus()
+    }
+
 }
