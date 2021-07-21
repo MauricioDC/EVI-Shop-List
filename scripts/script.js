6541c54b74e272c9bc2ef3451dc41a3f4e87d7cf
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-07-21 08:54:43 -0300

    Adicionando botão para editar quantidade

--- scripts/script.js
+++ scripts/script.js
@@ -10,33 +10,62 @@ let qtdProd = document.getElementById("qtdProduto")
 let lista = []
 
 document.getElementById("qtdProduto")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("buttonADC").click();
-    }
-});
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("buttonADC").click();
+        }
+    });
 
 function montarLista(produto) {
-        let tBody = document.querySelector("tbody")
-        let qtd = document.createElement("td")
-        let nome = document.createElement("td")
-        let linha = document.createElement("tr")
-        let remover = document.createElement("button")
-        nome.innerHTML = produto.nomeProduto
-        qtd.innerHTML =  produto.qtdProduto
-        linha.appendChild(nome)
-        linha.appendChild(qtd)
-        tBody.appendChild(linha)
+    let tBody = document.querySelector("tbody")
+    let qtd = document.createElement("td")
+    let nome = document.createElement("td")
+    let qMenosL = document.createElement("td")
+    let qMaisL = document.createElement("td")
+    let linha = document.createElement("tr")
+    let qMenosB = document.createElement("button")
+    let qMaisB = document.createElement("button")
+
+    nome.innerHTML = produto.nomeProduto.toUpperCase()
+    qtd.innerHTML = produto.qtdProduto
+    qMenosB.innerText = "-"
+    qMaisB.innerText = "+"
+    qMaisB.addEventListener("click", function (e) {
+        produto.qtdProduto = parseInt(produto.qtdProduto) + 1
+        qtd.innerHTML = produto.qtdProduto
+    })
+
+    qMenosB.addEventListener("click", function (e) {
+        if (produto.qtdProduto <= 1) {
+            let confirm = window.confirm("Deseja remover o iten?")
+            if (confirm === true) {
+                let linha = document.querySelector("tr")
+               let tbody = document.querySelector("tbody")
+               tbody.deleteRow(linha);
+            }
+        } else {
+            produto.qtdProduto = parseInt(produto.qtdProduto) - 1
+            qtd.innerHTML = produto.qtdProduto
+        }
+
+    })
+    qMenosL.appendChild(qMenosB)
+    qMaisL.appendChild(qMaisB)
+    linha.appendChild(nome)
+    linha.appendChild(qtd)
+    linha.appendChild(qMaisL)
+    linha.appendChild(qMenosL)
+    tBody.appendChild(linha)
 }
 
 function addProduto() {
 
-    if(nmProd.value.length == 0){
+    if (nmProd.value.length == 0) {
         window.alert("Favor informar o nome do produto!")
         nmProd.focus()
-    } 
-    else if (qtdProd.value == 0 || qtdProd.value < 0){
+    }
+    else if (qtdProd.value == 0 || qtdProd.value < 0) {
         window.alert("Favor informar a quantidade!")
         qtdProd.focus()
     } else {
@@ -44,7 +73,6 @@ function addProduto() {
         lista.push(produto)
         nmProd.value = ""
         qtdProd.value = ""
-        console.log(JSON.stringify(produto))
         nmProd.focus()
         montarLista(produto)
     }
