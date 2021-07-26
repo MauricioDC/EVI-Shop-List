33cb862be85e3d55099abac127b050207ef6d027
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-07-26 16:46:48 -0300

    Ajustando pastas

---
+++ scripts/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const uri = 'mongodb+srv://hoods:delta1992@cluster0.gvdx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.error(err)
+    db = client.db('cluster2')
+    app.listen(3000, function () {
+        console.log('servidor rodando na porta 3000')
+    })
+
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('index.ejs')
+})
+
+app.post('/show', (req, res) => {
+    db.collection('data').insertOne(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('salvo no banco de dados')
+        res.redirect('/')
+    })
+})
